772fb2294bf9c4d5774c0d850ae8b2e3bd02dd46
uday-ravi <145611148+uday-ravi@users.noreply.github.com>
2024-03-17 18:22:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 import streamlit as st
 
-def main():
-    st.set_page_config(page_title="Ask your csv")
-    st.header("Ask your csv")
-
-    user_csv = st.file_uploader("upload your CSV file", type="csv")
-
+st.write("hi)
